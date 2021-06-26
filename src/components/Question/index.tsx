d2badb9e454f76df29b597d873440f8dd7126583
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2021-06-26 20:33:51 -0300

    Fifth day finished

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -4,10 +4,17 @@ import { IQuestion } from '../../types';
 function Question({
   author,
   content,
+  isAnswered = false,
+  isHighlighted = false,
   children
 }: IQuestion) {
   return (
-    <div className="question">
+    <div
+      className={
+        `question ${isAnswered ? 'answered' : ''}
+        ${isHighlighted && !isAnswered ? 'highlighted' : ''}`
+      }
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -2,20 +2,32 @@ import { useParams, useHistory } from 'react-router-dom';
 
 import './styles.scss';
 import { RoomParams } from '../../types';
-//import { useAuth } from '../hooks/useAuth';
+import { database } from '../../services/firebase';
 import { useRoom } from '../../hooks/useRoom';
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question';
 import logoImg from '../../assets/images/logo.svg';
-import { database } from '../../services/firebase';
+import checkImg from '../../assets/images/check.svg';
+import answerImg from '../../assets/images/answer.svg';
 
 function AdminRoom() {
-  //const { user } = useAuth();
   const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { title, questions } = useRoom(roomId);
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    });
+  }
 
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
@@ -61,6 +73,22 @@ function AdminRoom() {
                 key={question.id}
                 {...question} 
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Check question" />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Highlight question" />
+                    </button>
+                  </>
+                )}
                 <button
                   className="delete-button"
                   type="button"
