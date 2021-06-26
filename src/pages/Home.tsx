4839bbfaa544182f5b6e2dc3f530e9c8b80acb5f
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2021-06-26 13:45:57 -0300

    Third day finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import { useState, FormEvent } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 import { Button } from '../components/Button';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,6 +12,7 @@ import googleIcon from '../assets/images/google-icon.svg';
 function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   function handleCreateRoom() {
     if (!user) {
@@ -17,6 +20,19 @@ function Home() {
     }
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === '') return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomRef.key}`);
   }
 
   return (
@@ -34,10 +50,12 @@ function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,35 @@ export type AuthContextProviderProps = {
   children: ReactNode;
 }
 
+export type RoomParams = {
+  id: string;
+}
+
+export type RoomCodeProps = {
+  code: string;
+}
+
+export type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}>
+
+export interface IQuestion {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}
+
 export interface IUser {
   id: string;
   name: string;
